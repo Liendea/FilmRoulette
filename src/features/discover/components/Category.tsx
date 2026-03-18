dc561ca02aac0a58dada48afb4cadc6e44e7fd8e
Linda <bengtsson-linda@outlook.com>
2026-03-18 18:12:15 +0100

    add filteroptions for minRating, change genre chips into multidropdown instead to save place and make the filtermodal cleaner

---
+++ src/features/discover/components/Category.tsx
@@ -0,0 +1,49 @@
+import { View, Text, StyleSheet, Pressable } from "react-native";
+
+type CategoryProps = {
+  type: "movie" | "tv";
+  setType: (newType: "movie" | "tv") => void;
+};
+
+export default function Category({ type, setType }: CategoryProps) {
+  return (
+    <>
+      {/* Typ av innehåll */}
+
+      <View style={styles.row}>
+        <Pressable
+          style={[styles.typeButton, type === "movie" && styles.selectedType]}
+          onPress={() => setType("movie")}
+        >
+          <Text style={styles.buttonText}>Filmer</Text>
+        </Pressable>
+        <Pressable
+          disabled={true}
+          style={[styles.typeButton, type === "tv" && styles.selectedType]}
+          onPress={() => setType("tv")}
+        >
+          <Text style={styles.buttonText}>Serier</Text>
+        </Pressable>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionTitle: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "600",
+    marginBottom: 10,
+  },
+  row: { flexDirection: "row", gap: 10 },
+  typeButton: {
+    flex: 1,
+    padding: 12,
+    backgroundColor: "#333",
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  selectedType: { backgroundColor: "#E50914" },
+  buttonText: { color: "#fff", fontWeight: "600" },
+});
---
+++ src/features/discover/components/MinRatingDropdown.tsx
@@ -0,0 +1,85 @@
+import { StyleSheet, View, Text } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+import { StarIcon } from "phosphor-react-native";
+
+<StarIcon color="yellow" weight="fill" size={20} />;
+const data = [
+  { label: "Alla betyg", value: "0" },
+  { label: "1+ ", value: "1" },
+  { label: "2+ ", value: "2" },
+  { label: "3+ ", value: "3" },
+  { label: "4+ ", value: "4" },
+  { label: "5+ ", value: "5" },
+  { label: "6+ ", value: "6" },
+  { label: "7+ ", value: "7" },
+  { label: "8+ ", value: "8" },
+];
+
+type MinRatingProps = {
+  minRating: number;
+  setMinRating: (value: number) => void;
+};
+
+const renderItem = (item: any) => {
+  return (
+    <View style={styles.item}>
+      {item.value !== "0" && (
+        <StarIcon color="#FFD700" weight="fill" size={18} />
+      )}
+      <Text style={styles.textItem}>{item.label}</Text>
+    </View>
+  );
+};
+
+export default function Category({ minRating, setMinRating }: MinRatingProps) {
+  return (
+    <Dropdown
+      style={styles.dropdown}
+      containerStyle={styles.itemContainer}
+      autoScroll={false}
+      data={data}
+      labelField="label"
+      valueField="value"
+      activeColor="#333"
+      value={minRating.toString()}
+      onChange={(item) => setMinRating(Number(item.value))}
+      renderItem={renderItem}
+      renderLeftIcon={() => (
+        <StarIcon
+          color="#FFD700"
+          weight="fill"
+          size={20}
+          style={{ marginRight: 10 }}
+        />
+      )}
+      selectedTextStyle={styles.selectedTextStyle}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  dropdown: {
+    height: 50,
+    backgroundColor: "#333",
+    borderRadius: 8,
+    paddingHorizontal: 15,
+  },
+  selectedTextStyle: {
+    color: "white",
+  },
+  itemContainer: {
+    backgroundColor: "#222",
+    borderRadius: 8,
+    borderWidth: 0,
+  },
+  item: {
+    padding: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+  },
+  textItem: {
+    fontSize: 16,
+    color: "#fff",
+  },
+});
